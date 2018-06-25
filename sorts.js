467b73b37844a13aa1375b17edc485de90ca1bf1
Miyamoto <hkanata@gmail.com>
2018-06-25 11:06:08 -0300

    Create Sort.js

---
+++ sorts.js
@@ -0,0 +1,15 @@
+<script>
+  var array 		  = [6,1,5,8,9,4,7,2]; //expected: [1, 2, 4, 5, 6, 7, 8, 9]
+  var escolhido = 0;	
+  var j				  = 0;
+  for( var i=1; i < array.length; i++ ){
+    escolhido = array[i];
+    j = i-1;
+    while(  (j >= 0) && (escolhido < array[j]) ){
+      array[j+1] = array[j];
+      j = j - 1;
+    }
+    array[j+1]=escolhido;
+  }
+  console.log(array);
+</script>

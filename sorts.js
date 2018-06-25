90329b424a7a63ef94068478d6877310e7423866
Miyamoto <hkanata@gmail.com>
2018-06-25 11:07:19 -0300

    updating variable styles

--- sorts.js
+++ sorts.js
@@ -1,15 +1,14 @@
-<script>
-  var array 		  = [6,1,5,8,9,4,7,2]; //expected: [1, 2, 4, 5, 6, 7, 8, 9]
-  var escolhido = 0;	
-  var j				  = 0;
-  for( var i=1; i < array.length; i++ ){
-    escolhido = array[i];
-    j = i-1;
-    while(  (j >= 0) && (escolhido < array[j]) ){
-      array[j+1] = array[j];
-      j = j - 1;
-    }
-    array[j+1]=escolhido;
+var array  = [6,1,5,8,9,4,7,2]; //expected: [1, 2, 4, 5, 6, 7, 8, 9]
+var chosen = 0;	
+var j			 = 0;
+
+for( var i=1; i < array.length; i++ ){
+  chosen = array[i];
+  j = i-1;
+  while(  (j >= 0) && (chosen < array[j]) ){
+    array[j+1] = array[j];
+    j = j - 1;
   }
-  console.log(array);
-</script>
+  array[j+1]=chosen;
+}
+console.log(array);
